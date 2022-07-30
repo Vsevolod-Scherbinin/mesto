91d568cda683ab28487351f842dff194e13da73d
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2022-07-30 23:24:55 +0300

    Щербинин В.О. Проектная работа №8 - v.4.1

--- src/script/utils/utils.js
+++ src/script/utils/utils.js
@@ -2,13 +2,12 @@ import PopupWithImage from '../components/PopupWithImage.js';
 import Card from '../components/Card.js';
 
 const popupImage = new PopupWithImage('.popup_type_image');
+popupImage.setEventListeners();
 export function handleCardClick(name, link) {
   popupImage.open(name, link);
-  popupImage.setEventListeners();
 }
 
 export function createCard(elem) {
-  const card = new Card(elem, '#elements__item',
-  handleCardClick);
+  const card = new Card(elem, '#elements__item', handleCardClick);
   return card.generateCard();
 }
